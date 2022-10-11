f90365f2cffd51028c16d3ece29655ade7df644e
Gabriel Alves Oliveira <111614350+oliveiraprg@users.noreply.github.com>
2022-10-10 23:00:42 -0300

    tratando as urls e removendo referencias internas

--- crawler.py
+++ crawler.py
@@ -1,9 +1,11 @@
 from bs4 import BeautifulSoup
 import urllib3
+from urllib.parse import urljoin
 
 
 def crawl(pagina):
     http = urllib3.PoolManager()
+    
     try:
         dados_pagina = http.request('GET', pagina)
     except:
@@ -12,7 +14,15 @@ def crawl(pagina):
     sopa = BeautifulSoup(dados_pagina.data, 'lxml')
     links = sopa.find_all('a')
     for link in links:
-        print(str(link.contents) + str(link.get('href')))
 
-
+        if 'href' in link.attrs:
+            url = urljoin(pagina, str(link.get('href')))
+            #if url != link.get('href'):
+            if url.find("'") != -1:
+                continue
+            print(url)
+            url = url.split('#')[0]
+            print(url)
+            
+                
 crawl('https://pt.wikipedia.org/wiki/Linguagem_de_programa%C3%A7%C3%A3o')

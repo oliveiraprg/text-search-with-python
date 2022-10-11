bd1169181ba800b58f49464176e16a6ee7247b15
Gabriel Alves Oliveira <111614350+oliveiraprg@users.noreply.github.com>
2022-10-10 23:31:31 -0300

    definindo profundidade de busca nas paginas

--- crawler.py
+++ crawler.py
@@ -3,26 +3,32 @@ import urllib3
 from urllib.parse import urljoin
 
 
-def crawl(pagina):
-    http = urllib3.PoolManager()
-    
-    try:
-        dados_pagina = http.request('GET', pagina)
-    except:
-        print('Erro ao abrir a pagina ' + pagina)
-        
-    sopa = BeautifulSoup(dados_pagina.data, 'lxml')
-    links = sopa.find_all('a')
-    for link in links:
-
-        if 'href' in link.attrs:
-            url = urljoin(pagina, str(link.get('href')))
-            #if url != link.get('href'):
-            if url.find("'") != -1:
-                continue
-            print(url)
-            url = url.split('#')[0]
-            print(url)
+def crawl(paginas, profundidade):
+    for i in range(profundidade):
+        novas_paginas = set()
+        for pagina in paginas:
+            http = urllib3.PoolManager()
             
+            try:
+                dados_pagina = http.request('GET', pagina)
+            except:
+                print('Erro ao abrir a pagina ' + pagina)
+                continue
                 
-crawl('https://pt.wikipedia.org/wiki/Linguagem_de_programa%C3%A7%C3%A3o')
+            sopa = BeautifulSoup(dados_pagina.data, 'lxml')
+            links = sopa.find_all('a')
+            for link in links:
+        
+                if 'href' in link.attrs:
+                    url = urljoin(pagina, str(link.get('href')))
+                    #if url != link.get('href'):
+                    if url.find("'") != -1:
+                        continue
+                    url = url.split('#')[0]
+                    if url[0:4] == 'http':
+                        novas_paginas.add(url)
+            paginas = novas_paginas    
+            
+            
+lista_paginas = ['https://pt.wikipedia.org/wiki/Linguagem_de_programa%C3%A7%C3%A3o']
+crawl(lista_paginas, 2)

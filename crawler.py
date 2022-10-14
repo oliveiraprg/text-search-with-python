b1f23e7a8dd4cd36b84b02e45ec83bd06843d5a6
Gabriel Alves Oliveira <111614350+oliveiraprg@users.noreply.github.com>
2022-10-14 10:44:08 -0300

    ajustando para padrão pep8

--- crawler.py
+++ crawler.py
@@ -9,140 +9,146 @@ from secret_settings import password_db
 
 def insere_palavra_localizacao(id_url, id_palavra, localizacao):
     global password_db
-    conexao = pymysql.connect(host='localhost', user='root',passwd=password_db,
-                              port=3306, db='indice', autocommit=True) 
+    conexao = pymysql.connect(host='localhost', user='root', passwd=password_db,
+                              port=3306, db='indice', autocommit=True)
     cursor = conexao.cursor()
-    cursor.execute('INSERT INTO palavra_localizacao (idurl, idpalavra, localizacao) VALUES (%s, %s, %s)', (id_url, id_palavra, localizacao))
+    cursor.execute('INSERT INTO palavra_localizacao (idurl, idpalavra, localizacao) VALUES (%s, %s, %s)',
+                   (id_url, id_palavra, localizacao))
     id_palavra_localizacao = cursor.lastrowid
     cursor.close()
     conexao.close()
-    
+
     return id_palavra_localizacao
 
 
 def insere_palavra(palavra):
     global password_db
-    conexao = pymysql.connect(host='localhost', user='root',passwd=password_db,
+    conexao = pymysql.connect(host='localhost', user='root', passwd=password_db,
                               port=3306, db='indice', autocommit=True,
-                              use_unicode=True, charset='utf8mb4') 
+                              use_unicode=True, charset='utf8mb4')
     cursor = conexao.cursor()
     cursor.execute('INSERT INTO palavras (palavra) VALUES (%s)', palavra)
     id_palavra = cursor.lastrowid
     cursor.close()
     conexao.close()
-    
+
     return id_palavra
 
 
 def insere_url_ligacao(id_url_origem, id_url_destino):
     global password_db
-    conexao = pymysql.connect(host='localhost', user='root',passwd=password_db,
+    conexao = pymysql.connect(host='localhost', user='root', passwd=password_db,
                               port=3306, db='indice', autocommit=True,
-                              use_unicode=True, charset='utf8mb4') 
+                              use_unicode=True, charset='utf8mb4')
     cursor = conexao.cursor()
-    cursor.execute('INSERT INTO url_ligacao (idurl_origem, idurl_destino) VALUES (%s, %s)', (id_url_origem, id_url_destino))
+    cursor.execute('INSERT INTO url_ligacao (idurl_origem, idurl_destino) VALUES (%s, %s)',
+                   (id_url_origem, id_url_destino))
     id_url_ligacao = cursor.lastrowid
     cursor.close()
     conexao.close()
-    
+
     return id_url_ligacao
 
 
 def insere_url_palavra(id_palavra, id_url_ligacao):
     global password_db
-    conexao = pymysql.connect(host='localhost', user='root',passwd=password_db,
+    conexao = pymysql.connect(host='localhost', user='root', passwd=password_db,
                               port=3306, db='indice', autocommit=True,
-                              use_unicode=True, charset='utf8mb4') 
+                              use_unicode=True, charset='utf8mb4')
     cursor = conexao.cursor()
-    cursor.execute('INSERT INTO url_palavra (idpalavra, idurl_ligacao) VALUES (%s, %s)', (id_palavra, id_url_ligacao))
+    cursor.execute('INSERT INTO url_palavra (idpalavra, idurl_ligacao) VALUES (%s, %s)',
+                   (id_palavra, id_url_ligacao))
     id_url_palavra = cursor.lastrowid
     cursor.close()
     conexao.close()
-    
+
     return id_url_palavra
 
 
 def get_url_ligacao(id_url_origem, id_url_destino):
     id_url_ligacao = -1
     global password_db
-    conexao = pymysql.connect(host='localhost', user='root',passwd=password_db,
-                              port=3306, db='indice') 
+    conexao = pymysql.connect(host='localhost', user='root', passwd=password_db,
+                              port=3306, db='indice')
     cursor = conexao.cursor()
-    cursor.execute('SELECT idurl_ligacao FROM url_ligacao WHERE idurl_origem = %s AND idurl_destino = %s', (id_url_origem, id_url_destino))
+    cursor.execute('SELECT idurl_ligacao FROM url_ligacao WHERE idurl_origem = %s AND idurl_destino = %s',
+                   (id_url_origem, id_url_destino))
     if cursor.rowcount > 0:
         id_url_ligacao = cursor.fetchone()[0]
     cursor.close()
     conexao.close()
-    
+
     return id_url_ligacao
 
 
 def get_id_url(url):
-    id_url = -1 # Não existe a palavra no indice
+    id_url = -1  # Não existe a palavra no indice
     global password_db
-    conexao = pymysql.connect(host='localhost', user='root',passwd=password_db,
+    conexao = pymysql.connect(host='localhost', user='root', passwd=password_db,
                               port=3306, db='indice', autocommit=True,
-                              use_unicode=True, charset='utf8mb4') 
+                              use_unicode=True, charset='utf8mb4')
     cursor = conexao.cursor()
     cursor.execute('SELECT idurl FROM urls WHERE url = %s', url)
     if cursor.rowcount > 0:
         id_url = cursor.fetchone()[0]
     cursor.close()
     conexao.close()
-    
+
     return id_url
 
 
 def palavra_indexada(palavra):
-    id_palavra = -1 # Não existe a palavra no indice
+    id_palavra = -1  # Não existe a palavra no indice
     global password_db
-    conexao = pymysql.connect(host='localhost', user='root',passwd=password_db,
+    conexao = pymysql.connect(host='localhost', user='root', passwd=password_db,
                               port=3306, db='indice', autocommit=True,
-                              use_unicode=True, charset='utf8mb4') 
+                              use_unicode=True, charset='utf8mb4')
     cursor = conexao.cursor()
-    cursor.execute('SELECT idpalavra FROM palavras WHERE palavra = %s', palavra)
+    cursor.execute(
+        'SELECT idpalavra FROM palavras WHERE palavra = %s', palavra)
     if cursor.rowcount > 0:
         id_palavra = cursor.fetchone()[0]
     cursor.close()
     conexao.close()
-    
+
     return id_palavra
 
 
 def insere_pagina(url):
     global password_db
-    conexao = pymysql.connect(host='localhost', user='root',passwd=password_db,
+    conexao = pymysql.connect(host='localhost', user='root', passwd=password_db,
                               port=3306, db='indice', autocommit=True,
-                              use_unicode=True, charset='utf8mb4') 
+                              use_unicode=True, charset='utf8mb4')
     cursor = conexao.cursor()
     cursor.execute('INSERT INTO urls (url) VALUES (%s)', url)
     id_pagina = cursor.lastrowid
     cursor.close()
     conexao.close()
-    
+
     return id_pagina
-    
-    
+
+
 def pagina_indexada(url):
     global password_db
-    conexao = pymysql.connect(host='localhost', user='root',passwd=password_db,
-                              port=3306, db='indice')   
-    retorno = -1 # Não existe a pagina
+    conexao = pymysql.connect(host='localhost', user='root', passwd=password_db,
+                              port=3306, db='indice')
+    retorno = -1  # Não existe a pagina
     cursor_url = conexao.cursor()
     cursor_url.execute('SELECT idurl FROM urls WHERE url = %s', url)
     if cursor_url.rowcount > 0:
         idurl = cursor_url.fetchone()[0]
         cursor_palavra = conexao.cursor()
-        cursor_palavra.execute("SELECT idurl FROM palavra_localizacao WHERE idurl = %s", (idurl))
+        cursor_palavra.execute(
+            "SELECT idurl FROM palavra_localizacao WHERE idurl = %s", (idurl))
         if cursor_palavra.rowcount > 0:
-            retorno = -2 # Existe pagina com palavras cadastradas
+            retorno = -2  # Existe pagina com palavras cadastradas
         else:
-            retorno = idurl # existe a pagina sem palavras
+            retorno = idurl  # existe a pagina sem palavras
         cursor_palavra.close()
-    
+
     cursor_url.close()
     conexao.close()
-    
+
     return retorno
 
 
@@ -164,13 +170,17 @@ def url_ligacao_palavra(url_origem, url_destino):
     palavras = separa_palavras(texto_url)
     id_url_origem = get_id_url(url_origem)
     id_url_destino = get_id_url(url_destino)
-    if id_url_destino == -1: id_url_destino = insere_pagina(url_destino)
-    if id_url_origem == id_url_destino: return
-    if get_url_ligacao(id_url_origem, id_url_destino) > 0: return
+    if id_url_destino == -1:
+        id_url_destino = insere_pagina(url_destino)
+    if id_url_origem == id_url_destino:
+        return
+    if get_url_ligacao(id_url_origem, id_url_destino) > 0:
+        return
     id_url_ligacao = insere_url_ligacao(id_url_origem, id_url_destino)
     for palavra in palavras:
         id_palavra = palavra_indexada(palavra)
-        if id_palavra == -1: id_palavra = insere_palavra(palavra)
+        if id_palavra == -1:
+            id_palavra = insere_palavra(palavra)
         insere_url_palavra(id_palavra, id_url_ligacao)
 
 
@@ -184,12 +194,12 @@ def indexador(url, sopa):
     indexada = pagina_indexada(url)
     if indexada == -2:
         print('Url já cadastrada')
-        return 
+        return
     elif indexada == -1:
         id_nova_pagina = insere_pagina(url)
     elif indexada > 0:
         id_nova_pagina = indexada
-        
+
     texto = get_texto(sopa)
     palavras = separa_palavras(texto)
     for i in range(len(palavras)):
@@ -201,7 +211,7 @@ def indexador(url, sopa):
 
 
 def crawl(paginas, profundidade):
-    i=0
+    i = 0
     while i < profundidade:
         novas_paginas = set()
         try:
@@ -213,7 +223,7 @@ def crawl(paginas, profundidade):
                     except:
                         print('Erro ao abrir a url ' + pagina)
                         continue
-                        
+
                     sopa = BeautifulSoup(dados_pagina.data, 'lxml')
                     indexador(pagina, sopa)
                     links = sopa.find_all('a')
@@ -229,11 +239,12 @@ def crawl(paginas, profundidade):
         except:
             print('Erro ao abrir a pagina ' + pagina)
             continue
-        paginas = novas_paginas    
+        paginas = novas_paginas
         i += 1
     print('Todas paginas percorridas')
-        
-    
-lista_paginas = ['https://pt.wikipedia.org/wiki/Linguagem_de_programa%C3%A7%C3%A3o']
+
+
+lista_paginas = [
+    'https://pt.wikipedia.org/wiki/Linguagem_de_programa%C3%A7%C3%A3o']
 
 #crawl(lista_paginas, 2)

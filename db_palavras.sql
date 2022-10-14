05ab30487e27d7282034161c82e0b61c4ebc4018
Gabriel Alves Oliveira <111614350+oliveiraprg@users.noreply.github.com>
2022-10-14 00:23:49 -0300

    Update db_palavras.sql

--- db_palavras.sql
+++ db_palavras.sql
@@ -28,3 +28,24 @@ create index idx_palavra_localizacao on palavra_localizacao(idpalavra_localizaca
 alter database indice character set = utf8mb4 collate = utf8mb4_unicode_ci;
 alter table palavras convert to character set utf8mb4 collate utf8mb4_unicode_ci;
 alter table palavras modify column palavra varchar(200) character set utf8mb4 collate utf8mb4_unicode_ci;
+
+create table url_ligacao (
+	idurl_ligacao int not null auto_increment,
+    idurl_origem int not null,
+    idurl_destino int not null,
+    constraint pk_idurl_ligacao primary key (idurl_ligacao),
+    constraint fk_url_ligacao_idurl_origem foreign key (idurl_origem) references urls(idurl),
+    constraint fk_url_ligacao_idurl_destino foreign key (idurl_destino) references urls(idurl)
+);
+create index idx_url_ligacao_idurl_origem on url_ligacao (idurl_origem);
+create index idx_url_ligacao_idurl_destino on url_ligacao (idurl_destino);
+
+create  table url_palavra (
+	idurl_palavra int not null auto_increment,
+    idpalavra int not null,
+    idurl_ligacao int not null,
+    constraint pk_url_palavra_idurl_palavra primary key(idurl_palavra),
+    constraint fk_url_palavra_idpalavra foreign key (idpalavra) references palavras(idpalavra),
+    constraint fk_url_palavra_idurl_ligacao foreign key (idurl_ligacao) references url_ligacao(idurl_ligacao)
+);
+create index idx_url_palavra_idpalavra on url_palavra (idpalavra);
